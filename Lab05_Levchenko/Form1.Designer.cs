22e5a68d87949759a247e1fb29fe07b56ca3c014
Igor <igor.levchenko1092@gmail.com>
2022-12-13 15:49:31 +0200

    Lab 5

---
+++ Lab05_Levchenko/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace Lab05_Levchenko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tOutFilePath = new System.Windows.Forms.TextBox();
+            this.tInFilePath = new System.Windows.Forms.TextBox();
+            this.bInFile = new System.Windows.Forms.Button();
+            this.bOutFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rB_SHA512 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA384 = new System.Windows.Forms.RadioButton();
+            this.rB_MD5 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA1 = new System.Windows.Forms.RadioButton();
+            this.rB_HAVAL = new System.Windows.Forms.RadioButton();
+            this.rB_SHA256 = new System.Windows.Forms.RadioButton();
+            this.rB_CRC32 = new System.Windows.Forms.RadioButton();
+            this.rB_RIPEMD160 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelSizeIN = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelSizeOUT = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.bClean = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labeltime = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(50, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вхідний файл:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(40, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вихідний файл:";
+            // 
+            // tOutFilePath
+            // 
+            this.tOutFilePath.Location = new System.Drawing.Point(196, 71);
+            this.tOutFilePath.Multiline = true;
+            this.tOutFilePath.Name = "tOutFilePath";
+            this.tOutFilePath.Size = new System.Drawing.Size(368, 28);
+            this.tOutFilePath.TabIndex = 2;
+            // 
+            // tInFilePath
+            // 
+            this.tInFilePath.Location = new System.Drawing.Point(196, 30);
+            this.tInFilePath.Multiline = true;
+            this.tInFilePath.Name = "tInFilePath";
+            this.tInFilePath.Size = new System.Drawing.Size(368, 28);
+            this.tInFilePath.TabIndex = 3;
+            // 
+            // bInFile
+            // 
+            this.bInFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bInFile.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.bInFile.Location = new System.Drawing.Point(581, 30);
+            this.bInFile.Name = "bInFile";
+            this.bInFile.Size = new System.Drawing.Size(50, 28);
+            this.bInFile.TabIndex = 4;
+            this.bInFile.Text = "...";
+            this.bInFile.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.bInFile.UseVisualStyleBackColor = true;
+            this.bInFile.Click += new System.EventHandler(this.bInFile_Click);
+            // 
+            // bOutFile
+            // 
+            this.bOutFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bOutFile.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.bOutFile.Location = new System.Drawing.Point(581, 71);
+            this.bOutFile.Name = "bOutFile";
+            this.bOutFile.Size = new System.Drawing.Size(50, 31);
+            this.bOutFile.TabIndex = 5;
+            this.bOutFile.Text = "...";
+            this.bOutFile.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.bOutFile.UseVisualStyleBackColor = true;
+            this.bOutFile.Click += new System.EventHandler(this.bOutFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rB_SHA512);
+            this.groupBox1.Controls.Add(this.rB_SHA384);
+            this.groupBox1.Controls.Add(this.rB_MD5);
+            this.groupBox1.Controls.Add(this.rB_SHA1);
+            this.groupBox1.Controls.Add(this.rB_HAVAL);
+            this.groupBox1.Controls.Add(this.rB_SHA256);
+            this.groupBox1.Controls.Add(this.rB_CRC32);
+            this.groupBox1.Controls.Add(this.rB_RIPEMD160);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.groupBox1.Location = new System.Drawing.Point(44, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 185);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим роботи";
+            // 
+            // rB_SHA512
+            // 
+            this.rB_SHA512.AutoSize = true;
+            this.rB_SHA512.Location = new System.Drawing.Point(152, 147);
+            this.rB_SHA512.Name = "rB_SHA512";
+            this.rB_SHA512.Size = new System.Drawing.Size(88, 22);
+            this.rB_SHA512.TabIndex = 7;
+            this.rB_SHA512.TabStop = true;
+            this.rB_SHA512.Text = "SHA-512";
+            this.rB_SHA512.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA384
+            // 
+            this.rB_SHA384.AutoSize = true;
+            this.rB_SHA384.Location = new System.Drawing.Point(152, 109);
+            this.rB_SHA384.Name = "rB_SHA384";
+            this.rB_SHA384.Size = new System.Drawing.Size(88, 22);
+            this.rB_SHA384.TabIndex = 6;
+            this.rB_SHA384.TabStop = true;
+            this.rB_SHA384.Text = "SHA-384";
+            this.rB_SHA384.UseVisualStyleBackColor = true;
+            // 
+            // rB_MD5
+            // 
+            this.rB_MD5.AutoSize = true;
+            this.rB_MD5.Location = new System.Drawing.Point(10, 149);
+            this.rB_MD5.Name = "rB_MD5";
+            this.rB_MD5.Size = new System.Drawing.Size(61, 22);
+            this.rB_MD5.TabIndex = 3;
+            this.rB_MD5.TabStop = true;
+            this.rB_MD5.Text = "MD5";
+            this.rB_MD5.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA1
+            // 
+            this.rB_SHA1.AutoSize = true;
+            this.rB_SHA1.Location = new System.Drawing.Point(152, 32);
+            this.rB_SHA1.Name = "rB_SHA1";
+            this.rB_SHA1.Size = new System.Drawing.Size(72, 22);
+            this.rB_SHA1.TabIndex = 4;
+            this.rB_SHA1.TabStop = true;
+            this.rB_SHA1.Text = "SHA-1";
+            this.rB_SHA1.UseVisualStyleBackColor = true;
+            // 
+            // rB_HAVAL
+            // 
+            this.rB_HAVAL.AutoSize = true;
+            this.rB_HAVAL.Location = new System.Drawing.Point(10, 70);
+            this.rB_HAVAL.Name = "rB_HAVAL";
+            this.rB_HAVAL.Size = new System.Drawing.Size(126, 22);
+            this.rB_HAVAL.TabIndex = 1;
+            this.rB_HAVAL.TabStop = true;
+            this.rB_HAVAL.Text = "HashAlgorithm";
+            this.rB_HAVAL.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA256
+            // 
+            this.rB_SHA256.AutoSize = true;
+            this.rB_SHA256.Location = new System.Drawing.Point(152, 72);
+            this.rB_SHA256.Name = "rB_SHA256";
+            this.rB_SHA256.Size = new System.Drawing.Size(88, 22);
+            this.rB_SHA256.TabIndex = 5;
+            this.rB_SHA256.TabStop = true;
+            this.rB_SHA256.Text = "SHA-256";
+            this.rB_SHA256.UseVisualStyleBackColor = true;
+            // 
+            // rB_CRC32
+            // 
+            this.rB_CRC32.AutoSize = true;
+            this.rB_CRC32.Location = new System.Drawing.Point(10, 32);
+            this.rB_CRC32.Name = "rB_CRC32";
+            this.rB_CRC32.Size = new System.Drawing.Size(78, 22);
+            this.rB_CRC32.TabIndex = 0;
+            this.rB_CRC32.TabStop = true;
+            this.rB_CRC32.Text = "CRC32";
+            this.rB_CRC32.UseVisualStyleBackColor = true;
+            // 
+            // rB_RIPEMD160
+            // 
+            this.rB_RIPEMD160.AutoSize = true;
+            this.rB_RIPEMD160.Location = new System.Drawing.Point(10, 111);
+            this.rB_RIPEMD160.Name = "rB_RIPEMD160";
+            this.rB_RIPEMD160.Size = new System.Drawing.Size(111, 22);
+            this.rB_RIPEMD160.TabIndex = 2;
+            this.rB_RIPEMD160.TabStop = true;
+            this.rB_RIPEMD160.Text = "RIPEMD160";
+            this.rB_RIPEMD160.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelSizeIN);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.groupBox2.Location = new System.Drawing.Point(335, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 66);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Оцінка розміру вхідного файлу (МБайт):";
+            // 
+            // labelSizeIN
+            // 
+            this.labelSizeIN.AutoSize = true;
+            this.labelSizeIN.Location = new System.Drawing.Point(18, 32);
+            this.labelSizeIN.Name = "labelSizeIN";
+            this.labelSizeIN.Size = new System.Drawing.Size(24, 18);
+            this.labelSizeIN.TabIndex = 0;
+            this.labelSizeIN.Text = "??";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelSizeOUT);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.groupBox3.Location = new System.Drawing.Point(335, 232);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(313, 72);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Оцінка розміру вихідного файлу (Байти):";
+            // 
+            // labelSizeOUT
+            // 
+            this.labelSizeOUT.AutoSize = true;
+            this.labelSizeOUT.Location = new System.Drawing.Point(18, 37);
+            this.labelSizeOUT.Name = "labelSizeOUT";
+            this.labelSizeOUT.Size = new System.Drawing.Size(24, 18);
+            this.labelSizeOUT.TabIndex = 0;
+            this.labelSizeOUT.Text = "??";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Автор";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Опис";
+            // 
+            // bStart
+            // 
+            this.bStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bStart.Location = new System.Drawing.Point(364, 327);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(114, 37);
+            this.bStart.TabIndex = 11;
+            this.bStart.Text = "Виконати";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bClean
+            // 
+            this.bClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bClean.Location = new System.Drawing.Point(517, 327);
+            this.bClean.Name = "bClean";
+            this.bClean.Size = new System.Drawing.Size(114, 37);
+            this.bClean.TabIndex = 12;
+            this.bClean.Text = "Очистити";
+            this.bClean.UseVisualStyleBackColor = true;
+            this.bClean.Click += new System.EventHandler(this.bClean_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Тривалість кодування:";
+            // 
+            // labeltime
+            // 
+            this.labeltime.AutoSize = true;
+            this.labeltime.Location = new System.Drawing.Point(222, 311);
+            this.labeltime.Name = "labeltime";
+            this.labeltime.Size = new System.Drawing.Size(62, 16);
+            this.labeltime.TabIndex = 14;
+            this.labeltime.Text = "00:00.000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 414);
+            this.Controls.Add(this.labeltime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.bClean);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bOutFile);
+            this.Controls.Add(this.bInFile);
+            this.Controls.Add(this.tInFilePath);
+            this.Controls.Add(this.tOutFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tOutFilePath;
+        private System.Windows.Forms.TextBox tInFilePath;
+        private System.Windows.Forms.Button bInFile;
+        private System.Windows.Forms.Button bOutFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rB_SHA512;
+        private System.Windows.Forms.RadioButton rB_SHA384;
+        private System.Windows.Forms.RadioButton rB_SHA256;
+        private System.Windows.Forms.RadioButton rB_SHA1;
+        private System.Windows.Forms.RadioButton rB_MD5;
+        private System.Windows.Forms.RadioButton rB_RIPEMD160;
+        private System.Windows.Forms.RadioButton rB_HAVAL;
+        private System.Windows.Forms.RadioButton rB_CRC32;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bClean;
+        private System.Windows.Forms.Label labelSizeIN;
+        private System.Windows.Forms.Label labelSizeOUT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labeltime;
+    }
+}
+
---
+++ Lab05_Levchenko/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab05_Levchenko
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelSizeIN.Text = "";
+            labelSizeOUT.Text = "";
+            labeltime.Text = "";
+        }
+
+        private byte[] myEncoding(byte[] inArr)
+        {
+            byte[] result = { 0 };
+
+            if (rB_CRC32.Checked == true)
+            {
+                uint a = Crc.Crc32(inArr);
+                return BitConverter.GetBytes(a);
+            }
+            if (rB_HAVAL.Checked == true)
+            {
+                HashAlgorithm sha = KeyedHashAlgorithm.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_RIPEMD160.Checked == true)
+            {
+                HashAlgorithm sha = RIPEMD160.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_MD5.Checked == true)
+            {
+                HashAlgorithm sha = MD5.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA1.Checked == true)
+            {
+                HashAlgorithm sha = SHA1.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA256.Checked == true)
+            {
+                HashAlgorithm sha = SHA256.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA384.Checked == true)
+            {
+                HashAlgorithm sha = SHA384.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA512.Checked == true)
+            {
+                HashAlgorithm sha = SHA512.Create();
+                return sha.ComputeHash(inArr);
+            }
+
+            return result;
+        }
+
+        private void bClean_Click(object sender, EventArgs e)
+        {
+            tInFilePath.Text = "";
+            tOutFilePath.Text = "";
+            labelSizeIN.Text = "";
+            labelSizeOUT.Text = "";
+            labeltime.Text = "";
+        }
+
+        static String BytesToString(long byteCount)
+        {
+            string[] suf = { "Byt", "KB", "MB", "GB", "TB", "PB", "EB" }; //
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        private void bInFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileInPath = openFileDialog.FileName;
+                    tInFilePath.Text = fileInPath;
+                }
+            }
+        }
+
+        private void bOutFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileOutPath = saveFileDialog.FileName;
+                tOutFilePath.Text = fileOutPath;
+            }
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            byte[] inArr = File.ReadAllBytes(tInFilePath.Text);
+
+
+            byte[] outArr = myEncoding(inArr);
+
+
+            stopwatch.Stop();
+            labeltime.Text = stopwatch.Elapsed.ToString(@"mm\:ss\.fff");
+
+            File.WriteAllBytes(tOutFilePath.Text, outArr);
+
+
+            string result = BitConverter.ToString(outArr).Replace("-", "");
+            MessageBox.Show("У файлі записане число (контрольна сума вхідного файлу):\n" + result, "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            string fileInPath = tInFilePath.Text;
+            long byteCountIn = new System.IO.FileInfo(fileInPath).Length;
+            string bytesizeIn = BytesToString(byteCountIn);
+            labelSizeIN.Text = bytesizeIn;
+
+            string fileOUTPath = tOutFilePath.Text;
+            long byteCountOut = new System.IO.FileInfo(fileOUTPath).Length;
+            string bytesizeOUT = BytesToString(byteCountOut);
+            labelSizeOUT.Text = bytesizeOUT;
+        }
+    }
+}
